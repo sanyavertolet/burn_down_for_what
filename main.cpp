f2ace7217085666067b58753e3e45bdf448c171f
sanyavertolet <lxnfrolov@gmail.com>
2022-11-15 10:24:57 +0300

    Multi-threaded version
    ### What's done:
     * Added wrappers for algorithm to run it in several threads

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ std::vector<int> get_possible_n_jobs();
 
 
 template<typename TemperatureDecreaser>
-void read_run_and_save_results(const std::string& filename, std::ofstream& out) {
+void single_read_run_and_save_results(const std::string& filename, std::ofstream& out) {
     static_assert(std::is_base_of<AbstractTemperatureDecreaser, TemperatureDecreaser>::value);
     std::vector<int> job_durations;
     int n_proc;
@@ -38,7 +38,27 @@ void read_run_and_save_results(const std::string& filename, std::ofstream& out) 
     out << n_proc << "," << n_jobs << "," << static_cast<long double>(duration) / (long double)(5.0) << "," << sum_quality / 5.0 << std::endl;
 }
 
-int main() {
+template<typename TemperatureDecreaser>
+void multi_read_run_and_save_results(const std::string& filename, std::ofstream& out, int n_threads) {
+    static_assert(std::is_base_of<AbstractTemperatureDecreaser, TemperatureDecreaser>::value);
+    std::vector<int> job_durations;
+    int n_proc;
+    int n_jobs;
+    read_from_csv(filename, n_proc, n_jobs, job_durations);
+    int sum_quality = 0;
+    long long duration = 0;
+    for (int i = 0; i < ITERATIONS_FOR_TEST; ++i) {
+        Timer timer;
+        timer.tick();
+        auto [solution, quality] = multiprocess_runner<TemperatureDecreaser>(n_proc, n_jobs, job_durations, n_threads);
+        timer.tock();
+        sum_quality += quality;
+        duration += timer.template duration().count();
+    }
+    out << n_proc << "," << n_jobs << "," << static_cast<long double>(duration) / (long double)(5.0) << "," << sum_quality / 5.0 << std::endl;
+}
+
+void single_threaded_run() {
     auto possible_n_proc = get_possible_n_proc();
     auto possible_n_jobs = get_possible_n_jobs();
 
@@ -53,22 +73,66 @@ int main() {
     std::ofstream mixed(mixed_report_file_name);
 
     for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
-        read_run_and_save_results<BoltzmannTemperatureDecreaser>(filename.path().string(), boltzmann);
+        single_read_run_and_save_results<BoltzmannTemperatureDecreaser>(filename.path().string(), boltzmann);
     }
 
     boltzmann.close();
 
     for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
-        read_run_and_save_results<CauchyTemperatureDecreaser>(filename.path().string(), cauchy);
+        single_read_run_and_save_results<CauchyTemperatureDecreaser>(filename.path().string(), cauchy);
     }
 
     cauchy.close();
 
     for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
-        read_run_and_save_results<MixedTemperatureDecreaser>(filename.path().string(), mixed);
+        single_read_run_and_save_results<MixedTemperatureDecreaser>(filename.path().string(), mixed);
     }
 
     mixed.close();
+}
+
+void multi_threaded_run(int n_threads) {
+    auto possible_n_proc = get_possible_n_proc();
+    auto possible_n_jobs = get_possible_n_jobs();
+
+    std::string dirname = "csv";
+
+    std::string boltzmann_report_file_name = "boltzmann.csv";
+    std::string cauchy_report_file_name = "cauchy.csv";
+    std::string mixed_report_file_name = "mixed.csv";
+
+    std::ofstream boltzmann(boltzmann_report_file_name);
+    std::ofstream cauchy(cauchy_report_file_name);
+    std::ofstream mixed(mixed_report_file_name);
+
+    for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
+        multi_read_run_and_save_results<BoltzmannTemperatureDecreaser>(filename.path().string(), boltzmann, n_threads);
+    }
+
+    boltzmann.close();
+
+    for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
+        multi_read_run_and_save_results<CauchyTemperatureDecreaser>(filename.path().string(), cauchy, n_threads);
+    }
+
+    cauchy.close();
+
+    for (const auto& filename : std::filesystem::directory_iterator(dirname)) {
+        multi_read_run_and_save_results<MixedTemperatureDecreaser>(filename.path().string(), mixed, n_threads);
+    }
+
+    mixed.close();
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        single_threaded_run();
+    } else if (argc == 3 && std::string(argv[1]) == "-t") {
+        int n_threads = std::stoi(std::string(argv[2]));
+        multi_threaded_run(n_threads);
+    } else {
+        std::cerr << "Usage: burn_down_for_what [-t NUM]" << std::endl;
+    }
 
     return 0;
 }
--- src/SASolver/ScheduleSASolver/ScheduleSASolver.h
+++ src/SASolver/ScheduleSASolver/ScheduleSASolver.h
@@ -6,6 +6,8 @@
 #define BURN_DOWN_FOR_WHAT_SCHEDULESASOLVER_H
 
 #include <utility>
+#include <thread>
+#include <mutex>
 
 #include "../AbstractSASolver/AbstractSASolver.h"
 #include "../../Solution/ScheduleSASolution/ScheduleSASolution.h"
@@ -51,5 +53,41 @@ private:
     int n_jobs;
 };
 
+
+std::mutex mutex;
+template<class TemperatureDecreaser>
+void solve_wrapper(ScheduleSASolver<TemperatureDecreaser> solver, ScheduleSASolution &best_solution, long long &best_quality) {
+    auto [solution, quality] = solver.solve();
+    mutex.lock();
+    if (best_quality > quality) {
+        best_solution = solution;
+        best_quality = quality;
+    }
+    mutex.unlock();
+}
+
+template<class TemperatureDecreaser>
+std::pair<ScheduleSASolution, long long> multiprocess_runner(int n_proc, int n_jobs, std::vector<int> durations, int n_threads) {
+    assert(n_threads <= std::thread::hardware_concurrency() && "Too many threads requested.");
+    using Solver = ScheduleSASolver<TemperatureDecreaser>;
+    std::vector<Solver> solvers;
+    std::vector<std::thread> thread_pull;
+
+    ScheduleSASolution best_solution(n_proc, n_jobs);
+    long long best_quality = LONG_LONG_MAX;
+
+    for(int i = 0; i < n_threads; ++i) {
+        solvers.push_back(Solver(n_proc, n_jobs, durations));
+        thread_pull.push_back(
+                std::thread(solve_wrapper<TemperatureDecreaser>, solvers.back(), std::ref(best_solution), std::ref(best_quality))
+                );
+    }
+
+    for(auto &thread : thread_pull) {
+        thread.join();
+    }
+
+    return { best_solution, best_quality };
+}
 
 #endif //BURN_DOWN_FOR_WHAT_SCHEDULESASOLVER_H
